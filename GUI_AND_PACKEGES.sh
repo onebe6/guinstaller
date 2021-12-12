1e97e0ddfafb4bc9a268a6472b209881b829e3e8
onebe6 <83883307+onebe6@users.noreply.github.com>
2021-12-12 18:29:00 +0100

    Add files via upload

--- GUI_AND_PACKEGES.sh
+++ GUI_AND_PACKEGES.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Add sudo no password rights
-sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+sudo sed -i 's/%wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL \n Defaults !tty_tickets/' /etc/sudoers
 
 for PKG in $(cat PKGS_BASIC.txt); do
     echo "DOWNLOADING: $PKG"
     sudo pacman -S $PKG --noconfirm --needed
 done
 
-echo "exec i3" | ~/.xinitrc
+echo "exec i3" | cat > ~/.xinitrc
 
 sudo systemctl enable lightdm.sevice
-
-# Remove no password rights
-sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
