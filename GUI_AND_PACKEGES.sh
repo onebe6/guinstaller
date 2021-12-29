bd6ca740254098ef444e9ac16ab2b78d652bfec7
onebe6 <83883307+onebe6@users.noreply.github.com>
2021-12-29 20:09:49 +0100

    Add files via upload

--- GUI_AND_PACKEGES.sh
+++ GUI_AND_PACKEGES.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#sudo sed -i 's/%wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL \n Defaults !tty_tickets/' /etc/sudoers
-
 for PKG in $(cat PKGS_BASIC.txt); do
     echo "DOWNLOADING: $PKG"
     sudo pacman -S $PKG --noconfirm --needed
@@ -10,3 +8,32 @@ done
 echo "exec i3" | cat > ~/.xinitrc
 
 sudo systemctl enable lightdm
+
+# Graphics Drivers find and install
+#if lspci | grep -E "NVIDIA|GeForce"; then
+#    pacman -S nvidia --noconfirm --needed
+#	nvidia-xconfig
+if lspci | grep -E "Radeon"; then
+    sudo pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils --needed --noconfirm
+#elif lspci | grep -E "Integrated Graphics Controller"; then
+fi
+
+echo "RADV_PERFTEST=aco" | cat > /etc/environment
+
+cd ~
+mkdir Github
+cd ~/Github
+
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+#yay -S corectrl
+
+#cat corectrl.rules > /etc/polkit-1/rules.d/90-corectrl.rules
+
+#sed -i 's/#GRUB_CMDLINE_LINUX_DEFAULT=\GRUB_CMDLINE_LINUX_DEFAULT=amdgpu.ppfeaturemask=0xffffffff/'
+
+#grub-mkconfig -o /boot/grub/grub.cfg
+
+cat config > ~/.config/i3/config
+mv wallpaper.jpg ~/Pictures/wallpaper.jpg
+cat picom.conf ~/.config/picom/picom.conf
